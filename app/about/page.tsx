1f2b91e5da30771de8050c7bc3f95a990cf0ee65
Sitch196 <sichinava1996@gmail.com>
2024-02-26 20:28:22 +0400

    about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import Image from "next/image";
+import hero from "../../assets/heroimage.jpg";
 
-export default function page() {
+export default function Page() {
   return (
-    <div>
-      <h1>about page</h1>
+    <div className="container mx-auto bg-[whitemoke] p-4 max-w-screen-md h-screen">
+      <p className="text-justify hyphens-auto">
+        Welcome to Tbilisi Couch Surfers, your trusted companion in discovering
+        the heart of Georgia's capital. We specialize in connecting travelers
+        with welcoming locals, offering a unique and authentic stay in Tbilisi.
+        As a dedicated accommodation service, Tbilisi Couch Surfers goes beyond
+        the conventional, fostering cultural exchange and creating memorable
+        experiences.
+      </p>
+      <br />
+      <p className="text-justify hyphens-auto">
+        Our platform enables visitors to find a comfortable couch or spare room
+        while immersing themselves in the warmth of Georgian hospitality. With a
+        commitment to safety and quality, we take pride in being the best choice
+        for those seeking not just a place to stay, but a genuine connection to
+        Tbilisi's vibrant spirit.
+      </p>
+      <Image src={hero} alt="hero image" loading="lazy" />
     </div>
   );
 }
--- components/NewsLetterSignUp.tsx
+++ components/NewsLetterSignUp.tsx
@@ -1,7 +1,7 @@
 export default function NewsLetterSignUp() {
   return (
-    <section className="w-full py-12 md:py-24 lg:py-10 bg-black border-t border-white">
-      <div className="container flex flex-col items-center justify-center gap-4 px-4 text-center md:px-6">
+    <section className="w-full py-12 md:py-24 lg:py-10 flex flex-col items-center justify-center bg-black border-t border-white">
+      <div className="container  gap-4 px-4 text-center md:px-6">
         <div className="space-y-3 text-center">
           <h2 className="text-3xl text-white font-bold tracking-tighter sm:text-4xl md:text-5xl">
             Join Our Community
